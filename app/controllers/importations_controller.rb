24093e25fb86b79f5c7f922cca7e59bcff77afd9
cyril ferte <cyril@fertec.fr>
2016-12-05 09:27:42 +0000

    g importation model and controller

---
+++ app/controllers/importations_controller.rb
@@ -0,0 +1,2 @@
+class ImportationsController < ApplicationController
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161205092217) do
+ActiveRecord::Schema.define(version: 20161205092624) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -19,6 +19,12 @@ ActiveRecord::Schema.define(version: 20161205092217) do
     t.string   "first_name"
     t.string   "last_name"
     t.string   "email"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "importations", force: :cascade do |t|
+    t.string   "file"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
